8c46b5f9979f68a10fef545770dcee72ffbfba2f
Kenny Yong <homeyong@gmail.com>
2022-10-07 11:27:31 +0800

    Update readme and change test cases

--- src/index.spec.js
+++ src/index.spec.js
@@ -19,33 +19,33 @@ describe('BlockscanChat', () => {
     expect(typeof init).toBe('function');
   });
 
-  it('.init should throw an error if no BLOCKSCAN_CHAT_API_KEY is provided', () => {
+  it('.init should throw an error if no BLOCKSCAN_CHAT_API_KEY is provided', (apiKey) => {
     const {init} = BlockscanChat;
     // If no BLOCKSCAN_CHAT_API_KEY is provided, the init method should throw an error
-    if (!process.env.BLOCKSCAN_CHAT_API_KEY) {
+    if (!apiKey) {
       expect(() => init()).toThrow();
     }
   });
 
-  it('.init should throw an error if BLOCKSCAN_CHAT_API_KEY is declared but no value is provided', () => {
+  it('.init should throw an error if BLOCKSCAN_CHAT_API_KEY is declared but no value is provided', (apiKey) => {
     const {init} = BlockscanChat;
     // If no BLOCKSCAN_CHAT_API_KEY is provided, the init method should throw an error
-    if (process.env.BLOCKSCAN_CHAT_API_KEY === '') {
+    if (apiKey === '') {
       expect(() => init()).toThrow();
     }
   });
 
-  it('.init should throw an error if no BLOCKSCAN_CHAT_API_URL is provided', () => {
+  it('.init should throw an error if no BLOCKSCAN_CHAT_API_URL is provided', (apiUrl) => {
     const {init} = BlockscanChat;
-    if (!process.env.BLOCKSCAN_CHAT_API_URL) {
+    if (!apiUrl) {
       expect(() => init()).toThrow();
     }
   });
 
-  it('.init should throw an error if BLOCKSCAN_CHAT_API_URL is declared but no value is provided', () => {
+  it('.init should throw an error if BLOCKSCAN_CHAT_API_URL is declared but no value is provided', (apiUrl) => {
     const {init} = BlockscanChat;
     // If no BLOCKSCAN_CHAT_API_KEY is provided, the init method should throw an error
-    if (process.env.BLOCKSCAN_CHAT_API_URL === '') {
+    if (apiUrl === '') {
       expect(() => init()).toThrow();
     }
   });
